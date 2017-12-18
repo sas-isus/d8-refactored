26b73491ce89ec67b42ac25e0a71be1fe800684c
ꓛ㇜ay <cwells@cwells.org>
2017-12-18 16:33:47 -0500

    Adding simplesamlphp config pieces

--- sites/default/default.settings.php
+++ sites/default/default.settings.php
@@ -766,3 +766,8 @@ if (file_exists(__DIR__ . '/settings.pantheon.php')) {
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
+
+if (file_exists(__DIR__ . '/settings.simplesamlphp.php')) {
+  include __DIR__ . "/settings.simplesamlphp.php";
+}
+
