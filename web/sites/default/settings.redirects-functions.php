de448eab0a9b6db7a181d4c4b44b7481c17c5a61
ꓛ㇜ay <cwells@cwells.org>
2018-07-18 14:51:42 -0400

    refactors: isProxied check

--- web/sites/default/settings.redirects-functions.php
+++ web/sites/default/settings.redirects-functions.php
@@ -18,16 +18,26 @@ function isD8() {
 }
 
 function isProxied() {
+    // What does a site need to do differently if its proxied?
+
 	// HTTP_X_FORWARDED_HOST is undefined when coming in through the load-balancer
 	// HTTP_HOST == HTTP_X_FORWARDED_HOST for direct access to pantheon + via CDN
 	// via proxy, HTTP_HOST -> 'live-sas-school.pantheonsite.io' while
 	//   HTTP_X_FORWARDED_HOST -> 'www.sas.upenn.edu, live-sas-school.pantheonsite.io'
-	return(	   isset($_SERVER['HTTP_X_FORWARDED_HOST']) 
-        	&& ($_SERVER['HTTP_HOST'] != $_SERVER['HTTP_X_FORWARDED_HOST']));
+	
+	//return(	   isset($_SERVER['HTTP_X_FORWARDED_HOST']) 
+    //    	&& ($_SERVER['HTTP_HOST'] != $_SERVER['HTTP_X_FORWARDED_HOST']));
+    if(isset($SERVER['HTTP_X_FORWARDED_HOST'])) {
+    	return 1;
+    } else {
+    	return 0;
+    }
 }
 
 
 function getCanonicalHost() {
+    // The outside URL, e.g. for TLS and Shib
+    // Pantheon robots.txt disallows things
 	global $primary_domain;
 
 	$CanonicalHost = $_SERVER['HTTP_HOST'];
@@ -43,7 +53,9 @@ function getCanonicalHost() {
 }
 
 function getClientHost() {
-	if (   isset($_SERVER['HTTP_X_FORWARDED_HOST'])
+    // Really the CNAME for live
+    // For Shib, TLS
+	if (isset($_SERVER['HTTP_X_FORWARDED_HOST'])
 	    && preg_match('@^(.*?)(,|$)@',$_SERVER['HTTP_X_FORWARDED_HOST'],$match)
 	    && (count($match) > 1)) {
 		return($match[1]);
@@ -53,8 +65,9 @@ function getClientHost() {
 	}
 }
 
-
 function redirectTo($url) {
+    // REFACTOR
+    // Why are we doing redirects here?
 	if (extension_loaded('newrelic')) {
 		newrelic_name_transaction("redirect");
 	}
@@ -65,6 +78,8 @@ function redirectTo($url) {
 }
 
 function isHTTP() {
+    // Why are we checking this?
+    // What are we doing with this?
     // From https://pantheon.io/docs/domains/ 
     return (   !isset($_SERVER['HTTP_USER_AGENT_HTTPS']) 
             || $_SERVER['HTTP_USER_AGENT_HTTPS'] != 'ON'
