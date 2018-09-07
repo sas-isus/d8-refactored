01a334e2c5a51171585197ecdc27472641d09932
Clay Wells (Infosec) <clayw@sas.upenn.edu>
2018-09-07 12:56:09 -0400

    updates: removing the primary_domain, it is only needed for live sites will break multidevs

--- web/sites/default/default.settings.local.php
+++ web/sites/default/default.settings.local.php
@@ -24,15 +24,6 @@
 //$settings['reverse_proxy_addresses'] = array('128.91.219.96');
 
 /**
- * Set primary_domain
- *
- * This should be the domain name that a user would type as the URL and what
- * search engines should be spidering.
- */
-
-$primary_domain = 'www.CHANGE.upenn.edu';
-
-/**
  * Set simplesamlphp library directory
  */
 //echo "settings.local: setting simplesamlphp_dir<br>";
