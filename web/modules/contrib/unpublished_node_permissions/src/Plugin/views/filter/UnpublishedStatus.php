87bd3548778b9927b45e2660cf8b61454e47ec36
Clay Wells (Infosec) <clayw@sas.upenn.edu>
2018-09-19 15:56:34 -0400

    new: adds unpublished_node_permissions.

---
+++ web/modules/contrib/unpublished_node_permissions/src/Plugin/views/filter/UnpublishedStatus.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\unpublished_node_permissions\Plugin\views\filter;
+
+use Drupal\node\Entity\NodeType;
+use Drupal\node\Plugin\views\filter\Status;
+
+/**
+ * Filter by unpublished status.
+ *
+ * @ingroup views_filter_handlers
+ *
+ * @ViewsFilter("node_unpublished_status")
+ */
+class UnpublishedStatus extends Status {
+  public function query() {
+    $table = $this->ensureMyTable();
+
+    $account = \Drupal::currentUser();
+
+    $query_addition = [];
+    foreach (NodeType::loadMultiple() as $type) {
+      $type_id = $type->id();
+      if ($account->hasPermission("view $type_id unpublished content")) {
+        $query_addition[] = "$table.type = '$type_id'";
+      }
+    }
+
+    // Original node_status query.
+    $query = "$table.status = 1 OR ($table.uid = ***CURRENT_USER*** AND ***CURRENT_USER*** <> 0 AND ***VIEW_OWN_UNPUBLISHED_NODES*** = 1) OR ***BYPASS_NODE_ACCESS*** = 1";
+
+    // Add every allowed unpublished content type in an OR group.
+    // See \Drupal\node\Plugin\views\filter\Status.
+    if (count($query_addition)) {
+      $query .= ' OR ';
+      $query .= '(' . implode(' OR ', $query_addition) . ')';
+    }
+
+    $this->query->addWhereExpression($this->options['group'], $query);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheContexts() {
+    $contexts = parent::getCacheContexts();
+
+    $contexts[] = 'user.roles';
+
+    return $contexts;
+  }
+
+}
---
+++ web/modules/contrib/unpublished_node_permissions/src/UnpublishedNodePermissions.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\unpublished_node_permissions;
+
+use Drupal\node\Entity\NodeType;
+use Drupal\node\NodePermissions;
+
+/**
+ * Provides dynamic permissions for nodes of different types.
+ */
+class UnpublishedNodePermissions extends NodePermissions {
+  /**
+   * Returns a list of node permissions for a given node type.
+   *
+   * @param \Drupal\node\Entity\NodeType $type
+   *   The node type.
+   *
+   * @return array
+   *   An associative array of permission names and descriptions.
+   */
+  protected function buildPermissions(NodeType $type) {
+    $type_id = $type->id();
+    $type_params = ['%type_name' => $type->label()];
+
+    return [
+      "view $type_id unpublished content" => [
+        'title' => $this->t('%type_name: View unpublished content', $type_params),
+      ],
+    ];
+  }
+}
